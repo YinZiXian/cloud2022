61a4473a519516ea82c6ec877c0e61302e433321
YinZiXian <15525112406@163.com>
2022-03-07 22:30:11 +0800

    添加单机eureka

---
+++ cloud-eureka-server7001/src/main/java/com/zixy/springcloud/EurekaMain7001.java
@@ -0,0 +1,20 @@
+package com.zixy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * TODO
+ *
+ * @author: Zixy
+ * @since: 2022-03-07 22:21
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+        System.out.println("启动成功");
+    }
+}
