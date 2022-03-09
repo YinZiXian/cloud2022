694e6b3c71d4bda6834531ad43489641a16018ec
YinZiXian <15525112406@163.com>
2022-03-09 22:06:55 +0800

    订单和支付模块进单机eureka

--- cloud-provider-payment8001/src/main/java/com/zixy/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/zixy/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.zixy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * SpringBoot启动类
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2022/3/6 11:56
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
